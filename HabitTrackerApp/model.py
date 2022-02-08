9b3d1605ddad60150cdfad4735cd675ed79894c9
Stevie <pkka.armor@gmail.com>
2022-02-08 22:14:46 +1000

    working on model level on adding new user to app resources

--- HabitTrackerApp/model.py
+++ HabitTrackerApp/model.py
@@ -7,10 +7,11 @@ def upload_users():
     global users
     try:
         with open("users_info.json") as users_file:
-            users = (json.load(users_file))["users"]
-            return users
+            return((json.load(users_file))["users"])
     except OSError:
         return "File not found"
 
 def create_new_user(user_name):
+    if user_name not in users.keys(): users["username"] = user_name
+    else: return ("user already exists")
     print(users)
--- HabitTrackerApp/view.py
+++ HabitTrackerApp/view.py
@@ -23,9 +23,12 @@ class MainWindow(qtw.QMainWindow):
         user_menu = self.menuBar()
         set_user_menu = qtw.QMenu("&User", self)
         set_user_menu = self.add_users(set_user_menu)   
+        register_new_user_action = qtw.QAction("&New user...", self)
+        register_new_user_action.triggered.connect(self.create_new_user)
+        set_user_menu.addAction(register_new_user_action)
         if set_user_menu != None:
             user_menu.addMenu(set_user_menu)
-            self.setMenuBar(user_menu)
+        self.setMenuBar(user_menu)
 
     def add_users(self, user_menu):
         users = get_users_controller()
@@ -33,7 +36,8 @@ class MainWindow(qtw.QMainWindow):
             for user in users:
                 user_name = user["username"]
                 user_action = qtw.QAction(f"&{user_name}", self)
-                user_action.triggered.connect(lambda state, val=user_name: self.get_user_stats(val))
+                user_action.triggered.connect(lambda clicked, val=user_name: 
+                                                    self.get_user_stats(val))
                 user_menu.addAction(user_action)
             return user_menu
         else:   
@@ -45,8 +49,7 @@ class MainWindow(qtw.QMainWindow):
             sys.exit()
              
     def get_user_stats(self, user_name):
-        print(user_name)
-        print(f"hey {user_name}")
+        print(f"Gettig user info for {user_name}")
 
 app = qtw.QApplication([])
 app_window = MainWindow()
